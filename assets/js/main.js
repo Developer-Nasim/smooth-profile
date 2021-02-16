fa754df61a524557f4b9b678f98ce8a2e9b630a7
Developer-Nasim <unique0coder@gmail.com>
2021-02-17 01:40:33 +0600

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){ 
+        $('select').niceSelect();  
+        $('.mfp-btn').magnificPopup({
+            type: 'image' 
+          }); 
+    });
+  
+}(jQuery));	
